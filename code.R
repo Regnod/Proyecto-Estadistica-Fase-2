7441ecc6708bf15a5a76706393b0f551b488e9f3
Yumenio <alejandroandy447@gmail.com>
2021-12-16 21:46:02 -0500

    ...

--- code.R
+++ code.R
@@ -1,6 +1,8 @@
 data <- read.csv("./2014_nba_draft_combine.csv")
-data$id <- NULL
+data$X <- NULL
+data$Year <- NULL
 data$Player <- NULL
 data$Draft.pick <- NULL
 data$Bench <- NULL
 attach(data)
+
