86bfa462d860fe75b12715c2dffaae8f6c5b9884
Yumenio <alejandroandy447@gmail.com>
2021-12-17 22:52:11 -0500

    Vertical (Max) ANOVA fix

--- anova Vertical (Max).r
+++ anova Vertical (Max).r
@@ -4,6 +4,8 @@ data2013 <- read.csv("2013_nba_draft_combine.csv")
 data2014 <- read.csv("2014_nba_draft_combine.csv")
 data2015 <- read.csv("2015_nba_draft_combine.csv")
 
+
+## found a Shaphiro test working using the original data deleting the uncomplete rows, using the fixed ones fails by a slight edge
 variable <- c(rep('Vertical (Max) 2012',49), rep('Vertical (Max) 2014', 49), rep('Vertical (Max) 2015', 49))
 wear <- c(data2012$Vertical..Max., data2014$Vertical..Max., data2015$Vertical..Max.)
 
