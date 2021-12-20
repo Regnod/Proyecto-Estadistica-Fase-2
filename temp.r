2fe9ec85d92e219637e5ba0770a3e611229b5660
Yumenio <alejandroandy447@gmail.com>
2021-12-20 01:00:00 -0500

    wrote all the dimension reduction part, TODO the plotting

--- temp.r
+++ temp.r
@@ -1,6 +1,6 @@
 library(lmtest)
 
-data2014 <- read.csv("2014_nba_draft_combine_fixed.csv")
+data2014 <- read.csv("2014_nba_draft_combine.csv")
 
 # data2012 <- read.csv("2012_nba_draft_combine_fixed.csv")
 # data2013 <- read.csv("2013_nba_draft_combine_fixed.csv")
@@ -14,8 +14,21 @@ data2014$Draft.pick <- NULL
 data2014$Bench <- NULL
 
 
-# analisis de componentes principales
-acp <- prcomp(data2014, scale=TRUE)
-summary(acp)
-plot(acp)
-biplot(acp)
+## analisis de componentes principales
+# acp <- prcomp(data2014, scale=TRUE)
+# summary(acp)
+# plot(acp)
+# biplot(acp)
+
+
+## kmeans
+km <- kmeans(data2014, 3)
+print(km)
+plot(data2014, col=km$cluster)
+plot(data2014$Vertical..Max.Reach., data2014$Weight, col=km$cluster)
+
+# for(i in 1:length(data2014)){
+#   for(j in i:length(data2014)){
+#     plot(x<-data2014[[i]], y<-data2014[[j]])
+#   }
+# }
